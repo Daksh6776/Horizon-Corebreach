c61bda29c90a7c75e7ef72b4d8881f834a734805
Daksh6776 <djhhadiya@gmail.com>
2026-04-03 17:52:26 +0530

    Delete horizon_data/src/main directory

--- horizon_data/src/main/java/com/horizonpack/horizondata/blocks/materials/StoneBlock.java
+++
@@ -1,6 +0,0 @@
-package com.horizonpack.horizondata.blocks.materials;
-import net.minecraft.world.level.block.Block;
-
-public class StoneBlock extends Block {
-    public StoneBlock(Properties properties) { super(properties); }
-}
--- horizon_data/src/main/java/com/horizonpack/horizondata/systems/MaterialPropertyRegistry.java
+++
@@ -1,9 +0,0 @@
-package com.horizonpack.horizondata.systems;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class MaterialPropertyRegistry {
-    // TODO: Map materials to attributes like flammability, blast resistance, and structural weight
-    private static final Map<String, Object> PROPERTIES = new HashMap<>();
-}
